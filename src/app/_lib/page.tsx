4dc349a170266bde7195ba9c67321066f9b3a8ae
pivnati <natanmuleta911@gmail.com>
2025-06-17 09:16:23 -0700

    create the private folder using the underscore

---
+++ src/app/_lib/page.tsx
@@ -0,0 +1,6 @@
+export default function Page() {
+    return (
+        <div>
+            <h2>Welcome to the Hello World App!</h2>        </div>
+    );
+}
--- src/app/product/[productId]/reviews/[reviewId]/not-found.tsx
+++ src/app/product/[productId]/reviews/[reviewId]/not-found.tsx
@@ -1,8 +1,13 @@
+"use client";
+import { usePathname } from "next/navigation";
 export default function NotFound() {
+
+    const pathname = usePathname();
+    const productId = pathname.split('/')[2];
+    const reviewId = pathname.split('/')[4];
     return (
         <div>
-            <h1>Page Not Found</h1>
-            <p>The page you are looking for does not exist.</p>
+            <h2>Review {reviewId} not found for product {productId}</h2>
         </div>
     );
 }

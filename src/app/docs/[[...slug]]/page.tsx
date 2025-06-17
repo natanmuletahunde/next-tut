27e6d347187b58d6e9cda9f0d0d085c3e839540a
pivnati <natanmuleta911@gmail.com>
2025-06-16 18:49:26 -0700

    create the custom not found page

--- src/app/docs/[[...slug]]/page.tsx
+++ src/app/docs/[[...slug]]/page.tsx
@@ -7,11 +7,11 @@ export default async function Docs({
     const {slug} = await params;
     if(slug?.length ===2){
         return(
-            <h1>Viewing docs for features {slug[0]} concept {slug[1]}</h1>
+            <h1>Viewing docs for features {slug[0]} concept  {slug[1]}</h1>
         );
     }
     else if(slug?.length ===1){
-        return <h1>Viewing docs for features {slug[0]}</h1>
+        return <h1>Viewing docs   features  {slug[0] }</h1>
     }
     return <h1>Docs Home page</h1>
 }

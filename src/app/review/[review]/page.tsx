5f03e8c9e1bf0aab3147770136100c5b6f869c39
pivnati <natanmuleta911@gmail.com>
2025-06-16 22:37:23 -0700

    import the notfound

--- src/app/review/[review]/page.tsx
+++
@@ -1,9 +0,0 @@
-export default async function ProductDetails({
-    params,
-  }: {
-    params: Promise<{ reviewId: string }>;
-  }) {
-    const reviewId = (await params).reviewId;
-    return <h1>Details about Review {reviewId}</h1>;
-  }
-  

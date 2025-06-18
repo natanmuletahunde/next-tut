66b9952ab97fe2992ae9c630d1b161870d97d8b3
pivnati <natanmuleta911@gmail.com>
2025-06-18 09:48:41 -0700

    create  the navigation uinsg the Link

--- src/app/marketing/page.tsx
+++ src/app/marketing/page.tsx
@@ -1,3 +1,13 @@
+import Link from 'next/link';
+
 export default function Home(){
-    return <h1>Welcome Home!</h1>
+
+    return (
+        <>
+        <h1>Welcome Home!</h1>
+        <Link href="/counter">Counter</Link>
+        <Link href="/login">Login</Link>
+
+        </>
+    )
 }

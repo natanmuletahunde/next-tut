ba4d9bc9a5b8601bcd9101b77327fefe317ddde0
pivnati <natanmuleta911@gmail.com>
2025-06-19 10:06:44 -0700

    create the error page.tsx in the counter fodler

---
+++ src/app/counter/loading.tsx
@@ -0,0 +1,11 @@
+export default function  LoadingCounter() {
+    return (
+        <div>
+            <h1>
+                Loading Counter...
+            </h1>
+        </div>
+    )
+}
+
+
--- src/app/counter/page.tsx
+++ src/app/counter/page.tsx
@@ -1,6 +1,14 @@
-import {Counter} from './counter'
-export const metadata = {
-    title: 'Counter',}
-export default function CounterPage(){
-    return <Counter/>
+import { Metadata } from 'next'
+export const metadata:Metadata= {
+    title: {
+        absolute: "Counter",
+    },
+}
+export default async function Counter() {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  return (
+    <>
+      <h1>Welcome To counter  page!</h1>
+    </>
+  ); // Simulating a delay
 }

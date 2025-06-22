714569407ba76f86d6a832eef9be54673df414fd
pivnati <natanmuleta911@gmail.com>
2025-06-22 08:45:33 -0700

    create the unmatched route for the archived

--- src/app/complex-dashboard/@notifications/archived/page.tsx
+++ src/app/complex-dashboard/@notifications/archived/page.tsx
@@ -4,9 +4,9 @@ import  Link from "next/link";
 export default function ArchivedNotification(){
     return (
         <Card>
-            <h1 className="text-2xl font-bold mb-4">Archived Notifications</h1>
+             Archived Notifications
             <div>
-                <Link href ="/complex-dashboard/">
+                <Link href ="/complex-dashboard">
                 Default Page 
                  </Link>
             </div>
--- src/app/complex-dashboard/@notifications/page.tsx
+++ src/app/complex-dashboard/@notifications/page.tsx
@@ -1,9 +1,14 @@
 import { Card } from "../../../components/card";
-
+import Link from "next/link";
 export default function Notification() {
     return (
         <Card>
-                Notification Page
+                <div>
+                    Notification
+                </div>
+                <div>
+                <Link href="complex-dashboard/archived">Archived</Link>
+                </div>
         </Card>
     )
 }

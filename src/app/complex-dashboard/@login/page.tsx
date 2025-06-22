b62d8b88dd9c200af6c53af520259e251881ec80
pivnati <natanmuleta911@gmail.com>
2025-06-22 14:47:57 -0700

    create the  login page

---
+++ src/app/complex-dashboard/@login/page.tsx
@@ -0,0 +1,10 @@
+import { Card } from "@/components/card";
+export default function logIn(){
+    return (
+        <Card>
+              <div>
+                Login page 
+                </div>   
+        </Card>
+    )
+}

d510094fe92670b3794b1f01bc47dbec875dc173
pivnati <natanmuleta911@gmail.com>
2025-06-23 16:22:53 -0700

    create the intercepted route by (...)

---
+++ src/app/comments/page.tsx
@@ -0,0 +1,9 @@
+export default function CommentsPage() {    
+    return (
+        <div>
+            <h1>Comments Page</h1>
+            <p>This is the content of the Comments page.</p>
+            <p>Here you can view and manage comments.</p>
+        </div>
+    );
+}

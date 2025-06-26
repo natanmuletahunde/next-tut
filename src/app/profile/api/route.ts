4d91a7d49a466402c563a585deebc4604df90d55
pivnati <natanmuleta911@gmail.com>
2025-06-26 08:41:53 -0700

    create the api and modfiy the route.ts in the profile folder

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -4,3 +4,4 @@ export default async function GET(request: NextRequest) {
   console.log(requestHandlers.get('Authorization'));
   return new Response('Profile API data '); // Log the Authorization header
 }
+    
